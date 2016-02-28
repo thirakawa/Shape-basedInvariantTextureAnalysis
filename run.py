b29f11402c1b4abf108d9bb2084d7ffd29d56ebb
Tsubasa Hirakawa <hirakawa@eml.hiroshima-u.ac.jp>
2016-02-28 16:46:48 +0900

    edit gitignore and add "run.py" which extract SITA from multi-images.

---
+++ run.py
@@ -0,0 +1,38 @@
+
+import sys, os
+import SITA as sita
+
+if __name__ == '__main__':
+    import time
+    import itertools
+    from multiprocessing import Process
+
+    def applyAll(filenames):
+        for f in filenames:
+            if f == None:
+                break
+            print f.strip()
+            sita.SitaElements(f.strip(), filterSize=10, fcomb='SI')
+
+    f = open('files.txt')
+    lines = f.readlines()
+    f.close()
+
+    N_seg = 30
+
+    s = time.time()
+    threads = []
+    for files in itertools.izip_longest( *[iter(lines)] * (len(lines)/N_seg) ):
+        threads.append( Process(target=applyAll, args=(files, )) )
+
+    print "number of threads:", len(threads)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+    e = time.time()
+    print "extract SITA; done."
+    print "Processing time", (e-s)
+
